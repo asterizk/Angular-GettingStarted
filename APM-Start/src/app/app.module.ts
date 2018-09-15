3cf2c97cfc1338da7c7f7d20d6df8503e4b37b3e
Krishen Greenwell <asterizk@mac.com>
2018-09-15 15:04:33 -0400

    End of "More on Components > Building Custom Pipes"

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { ProductListComponent } from './products/product-list.component';
     FormsModule ],
   declarations: [
     AppComponent,
-    ProductListComponent ],
+    ProductListComponent,
+    ConvertToSpacesPipe ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
