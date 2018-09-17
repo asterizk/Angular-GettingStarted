641aaeaf168e8818718a663fb3550bfea19438dc
Krishen Greenwell <krishen.greenwell@cae.com>
2018-09-17 18:22:27 -0400

    End of "Building Nested Components > Passing Data to a Nested Component"

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   imports: [
@@ -13,7 +14,8 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
   declarations: [
     AppComponent,
     ProductListComponent,
-    ConvertToSpacesPipe ],
+    ConvertToSpacesPipe,
+    StarComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
