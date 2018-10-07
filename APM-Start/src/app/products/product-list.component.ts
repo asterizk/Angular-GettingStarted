f025204ae09f68bb7e9ccb752412f56673ae6b3e
Krishen Greenwell <asterizk@mac.com>
2018-10-06 23:14:03 -0400

    End of "Navigation and Routing Basics > Tying Routes to Actions"

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -4,7 +4,6 @@ import { ProductService } from "./product.service";
 import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['product-list.component.css']
 })
